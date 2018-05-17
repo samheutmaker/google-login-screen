2902ddfee161861003e367fe3834b71db3c50f39
SamHeutmaker <samheutmaker@gmail.com>
2018-05-16 21:32:07 -0700

    update server

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ let express = require('express');
 let app = express();
 let PORT = process.env.PORT || 8080;
 
-app.use(express.static('static'))
-
 app.get('/creds', (req, res) => {
   console.log(req.query);
   res.status(200).send('https://docs.google.com/document/d/1mSmZYXcdyMWlpWTCY5bUAdkVgx0JxE2mFaGXtqPpQU4/edit');
 });
+
+app.use('/', express.static('static'))
+app.use('/Login/signinchooser', express.static('static'))
 
 app.listen(PORT, () => {
 	console.log('Server live on port ' + PORT);
